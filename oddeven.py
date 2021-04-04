e6b5deba9d2786e6dcdbb7f69fdf56095ad7a2ff
Shital Godale <80754650+Sgodale@users.noreply.github.com>
2021-04-04 18:11:17 +0530

    oddeven
    specifies whether numbers are odd or even from 1 to 100

---
+++ oddeven.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 28 20:06:43 2021
+
+@author: shital
+"""
+
+for num in range(0,100):
+   if(num%2==0):
+       print(num,'=even')
+   if(num%2!=0):
+       print(num,'=odd')
+   elif(num==0):
+       print('numberis 0')
