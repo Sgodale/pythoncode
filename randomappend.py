624de22457296a696054e505c13e855e6f8d298f
Shital Godale <80754650+Sgodale@users.noreply.github.com>
2021-04-06 19:15:49 +0530

    creating random numbers list using append
    random.random() generates pseudo-random real numbers between 0 and 1. But what
    if you needed other random reals? Write a program to use only random.random()  
    to generate a list of random reals between 30 and 35. This is a simple matter
    of multiplication and addition. By multiplying you can spread the random numbers
    out to cover the range 0 to 5. By adding you can shift these numbers up to the 
    required range from 30 to 35.  Set the seed in this function to 70 so that 
    everyone generates the same random numbers and will agree with the grader's 
    list of random numbers. Print out the list (in list form).
    """

---
+++ randomappend.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 19:01:34 2021
+
+@author: sgodale
+"""
+#%%
+import random
+def problem2_4():
+    numlis=[]
+    random.seed(70)
+    for i in range(0,10):
+        num=(random.random()*5+30)
+        numlis.append(num)
+    print(numlis)
+    
+            
+    #%%
+     
+        
