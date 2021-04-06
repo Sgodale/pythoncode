92f95cee15b0ef037871a4f84628844c5e6e7ca6
Shital Godale <80754650+Sgodale@users.noreply.github.com>
2021-04-06 19:17:26 +0530

    random integer
    Let's do a small simulation. Suppose that you rolled a die repeatedly. Each 
    time that you roll the die you get a integer from 1 to 6, the number of pips
    on the die. Use random.randint(a,b) to simulate rolling a die 10 times and 
    printout the 10 outcomes. The function random.randint(a,b) will
    generate an integer (whole number) between the integers a and b inclusive.
    Remember each outcome is 1, 2, 3, 4, 5, or 6, so make sure that you can get 
    all of these outcomes and none other. Print the list, one item to a line so that
    there are 10 lines as in the example run.  Make sure that it has 10 items 
    and they are all in the range 1 through 6.  Here is one of my runs. In
    the problem below I ask you to set the seed to 171 for the benefit of the 
    auto-grader. In this example, that wasn't done and so your numbers will be 
    different.  Note that the seed must be set BEFORE randint is used.

---
+++ randomint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 19:20:23 2021
+
+@author: sgodale
+"""
+
+#%%
+import random
+def problem2_5():
+    random.seed(171)
+    for i in range(0,10):
+        print(random.randint(1,6))
+        
+    #%%
+        
