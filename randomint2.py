e05097fe6d82583754d9947069da8eca5f9b1cf0
Shital Godale <80754650+Sgodale@users.noreply.github.com>
2021-04-06 19:19:28 +0530

    2 dice rolled program
    Let's continue with our simulation of dice by rolling two of them. This time
    each die can come up with a number from 1 to 6, but you have two of them. The
    result or outcome is taken to be the sum of the pips on the two dice. Write a 
    program that will roll 2 dice and produce the outcome. This time let's roll 
    the two dice 100 times. Print the outcomes one outcome per line.

---
+++ randomint2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 19:29:47 2021
+
+@author: sgodale
+"""
+#%%
+import random
+def problem2_6():
+    random.seed(431)
+    for i in range(0,100):
+        print(random.randint(1,6)+random.randint(1,6))
+        
+        #%%
