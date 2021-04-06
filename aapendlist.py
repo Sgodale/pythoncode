02af08d937a9d75240ed10bdc2d343f796fff8bc
Shital Godale <80754650+Sgodale@users.noreply.github.com>
2021-04-06 19:05:27 +0530

    appendlist
    Write a function 'problem2_1()' that sets a variable lis = list(range(20,30)) and
    does all of the following, each on a separate line: 
    (a) print the element of lis with the index 3
    (b) print lis itself
    (c) write a 'for' loop that prints out every element of lis. Recall that 
        len() will give you the length of such a data collection if you need that.
        Use end=" " to put one space between the elements of the list lis.  Allow
        the extra space at the end of the list to stand, don't make a special case
        of it.

---
+++ aapendlist.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#%%
+def problem2_1():
+    numlis=[]
+    for i in range(20,30,1):
+       numlis.append(i)
+       
+    print(numlis[3])
+    print(numlis)
+    num=len(numlis)
+    for i in range(0,num):
+        print(numlis[i],end=' ')
+     
+   #%% 
+        
