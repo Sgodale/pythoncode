de8cfd56ab2402a283fb1c0910a4c70719dcb8a8
Shital Godale <80754650+Sgodale@users.noreply.github.com>
2021-04-06 19:26:53 +0530

    print a collection
    Below you see three objects which are of collection data type: a list, a tuple, 
    and a string. Although different in many ways, you can write a 'for' loop that 
    steps through a "collection" and it will work with all three. This problem
    is started for you. Finish it by writing the loop that will step through the
    collection and print out its items, one per line.

---
+++ printcollection.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 02:19:26 2021
+
+@author: sgodale
+"""
+#%%
+nlis = [23,64,23,46,12,24]          # list
+atup = ("c","e","a","d","b")        # tuple
+str1 = "Rumplestilskin"             # string
+def problem3_2(collection):
+    for i in range(0,len(collection)):
+        print(collection[i])
+        
+        #%%
+    
